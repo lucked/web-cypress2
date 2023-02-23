5885b01b33dfc1fbbd99b3d6896639dd81772519
Lucas rodrigues <lucasrodrigues@adopets.org>
2023-02-23 18:17:16 -0300

    reset

--- slack.js
+++ slack.js
@@ -1,6 +1,6 @@
 const { createReadStream } = require('fs');
 const { WebClient } = require('@slack/web-api');
-const token = 'xoxb-4813312330133-4828896589233-8eGEgZtEPn3XXvZ3yxQDzluO';
+const token = 'xoxb-4813312330133-4828896589233-40tqSYTjXIZ5hdknvgMmAsst';
 const channelId = 'C04Q040F36X';
 const web = new WebClient(token);
 var fs = require('fs');
@@ -23,5 +23,3 @@ fs.readdir(dirPath, function (err, files) {
     uploadFileToSlack();
   }
 });
-
-//https://hooks.slack.com/services/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
